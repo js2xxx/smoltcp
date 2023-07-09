84001fcf1adf471801ab14f2dc0782363b00a084
徐启航 <akucxy@163.com>
2023-07-09 20:00:09 +0800

    Fix behavior

--- src/iface/interface/mod.rs
+++ src/iface/interface/mod.rs
@@ -1246,12 +1246,19 @@ impl InterfaceInner {
         ip_payload: &'frame [u8],
     ) -> Option<IpPacket<'frame>> {
         #[cfg(feature = "socket-udp")]
-        for udp_socket in sockets
-            .items_mut()
-            .filter_map(|i| udp::Socket::downcast_mut(&mut i.socket))
         {
-            if udp_socket.accepts(self, &ip_repr, &udp_repr) {
-                udp_socket.process(self, meta, &ip_repr, &udp_repr, udp_payload);
+            let mut has_udp = false;
+            for udp_socket in sockets
+                .items_mut()
+                .filter_map(|i| udp::Socket::downcast_mut(&mut i.socket))
+            {
+                if udp_socket.accepts(self, &ip_repr, &udp_repr) {
+                    udp_socket.process(self, meta, &ip_repr, &udp_repr, udp_payload);
+                    has_udp = true;
+                }
+            }
+            if has_udp {
+                return None;
             }
         }
 
